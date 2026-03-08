1095b41e4e22b881fa26413682a9002482d5221f
yousufabdi <yufis0844@gmail.com>
2026-03-09 00:37:45 +0300

    prod api

--- frontend/src/store/api.ts
+++ frontend/src/store/api.ts
@@ -1,4 +1,5 @@
 // export const API =
 //   process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:3000/api";
+
 export const API =
   process.env.NEXT_PUBLIC_API_BASE ?? "http://44.200.167.113/api";
