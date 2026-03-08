88b931a43c636b6e6e775064569941e762c6aaab
yousufabdi <yufis0844@gmail.com>
2026-03-09 00:32:32 +0300

    prod api

--- frontend/src/store/api.ts
+++ frontend/src/store/api.ts
@@ -1,4 +1,4 @@
-export const API =
-  process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:3000/api";
 // export const API =
-//   process.env.NEXT_PUBLIC_API_BASE ?? "https://guulsync.onrender.com/api";
+//   process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:3000/api";
+export const API =
+  process.env.NEXT_PUBLIC_API_BASE ?? "http://44.200.167.113/api";
