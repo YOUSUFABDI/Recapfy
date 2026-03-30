587a6d99de5a4c867267915460bb003b2c84449f
YOUSUF ABDI <106746301+YOUSUFABDI@users.noreply.github.com>
2026-03-30 11:38:47 +0300

    Update Demo.tsx

--- frontend/src/sections/landing-page/Demo.tsx
+++ frontend/src/sections/landing-page/Demo.tsx
@@ -60,7 +60,7 @@ const Demo = () => {
           >
             <Box
               component="iframe"
-              src="https://www.youtube.com/embed/u31qwQUeGuM"
+              src="https://www.youtube.com/embed/wT-L5v3tUYY"
               title={`${CONFIG.appName} demo`}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
